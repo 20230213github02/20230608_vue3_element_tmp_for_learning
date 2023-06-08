c9cce3b8331f06af4ff4fe0cee2e790392c17267
20230516_macos_git_user <add-122@hotmail.com>
2023-06-08 14:57:48 +0900

    改了bug

--- 20230608_vue3_element_tmp_for_learning/src/components/0201_ichiran.vue
+++ 20230608_vue3_element_tmp_for_learning/src/components/0201_ichiran.vue
@@ -3,7 +3,6 @@
     <div>
         <h1></h1>
         <div>
-            <!-- {{ applications }} -->
             <div v-for="application in applications">
                 {{ application }}
             </div>
@@ -25,8 +24,8 @@
             <div class="demonstration">All combined</div>
             <el-pagination v-model:current-page="currentPage" v-model:page-size="applicationPageSize"
                 :page-sizes="[10, 20, 50, 100, 300, 400]" :small="small" :disabled="disabled" :background="background"
-                layout="total, sizes, prev, pager, next, jumper" :total="applicationPageCount" @size-change="handleSizeChange"
-                @current-change="handleCurrentChange" />
+                layout="total, sizes, prev, pager, next, jumper" :total="applicationCount"
+                @size-change="handleSizeChange" @current-change="handleCurrentChange" />
         </div>
         <el-form>
 
@@ -45,59 +44,26 @@ const applications = ref('');
 const notifacations = ref('');
 const applicationPageCount = ref('');
 const applicationPageSize = ref(10);
+const applicationCount = ref('');
 const currentPage = ref(1);
 const getApplicationPage = (pageNum) => {
     axios.get('http://localhost:8815/Application/page/' + pageNum + '/' + applicationPageSize.value).then(response => {
         console.log(response);
         applicationPageCount.value = response.data.pages;
+        applicationCount.value = response.data.total;
         applications.value = response.data.list;
     })
 };
 onMounted(() => {
-    // axios.get('http://localhost:8815/Application/all').then(response => {
-    //     // for test add by ZHY
-    //     // console.log(response)
-    //     if (response.status == '200') {
-    //         applications.value = response.data.data;
-
-    //         //for test add by ZHY
-    //         // console.log(this.applications);
-    //     }
-    // });
     axios.get('http://localhost:8815/Notification/all').then(response => {
-        // console.log(response);
         if (response.status == '200') {
             notifacations.value = response.data.data;
-            // console.log(this.notifacations);
         }
     });
     getApplicationPage(1);
 }
 );
 
-// export default {
-//     mounted() {
-
-
-//     },
-// methods: {
-//     getApplicationPage(pageNum) {
-//         axios.get('http://localhost:8815/Application/page/' + pageNum + '/' + this.applicationPageSize).then(response => {
-//             console.log(response);
-//             this.applicationPageCount = response.data.pages;
-//         })
-//     },
-
-//     }
-// }
-
-// const currentPage1 = ref(5)
-// const currentPage2 = ref(5)
-// const currentPage3 = ref(5)
-// const currentPage4 = ref(4)
-// const pageSize2 = ref(100)
-// const pageSize3 = ref(100)
-// const pageSize4 = ref(100)
 const small = ref(false)
 const background = ref(false)
 const disabled = ref(false)
@@ -111,4 +77,11 @@ const handleCurrentChange = (val) => {
     getApplicationPage(currentPage.value);
 }
 </script>
-<style></style>
+<style>
+.demo-pagination-block + .demo-pagination-block {
+  margin-top: 10px;
+}
+.demo-pagination-block .demonstration {
+  margin-bottom: 16px;
+}
+</style>
