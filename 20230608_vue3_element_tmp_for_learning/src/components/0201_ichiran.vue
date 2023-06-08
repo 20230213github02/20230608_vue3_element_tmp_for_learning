7009d785db5a2791b916427fb815c14131208053
20230516_macos_git_user <add-122@hotmail.com>
2023-06-08 14:34:51 +0900

    ichiran: comment unuseful code, add pagination code

--- 20230608_vue3_element_tmp_for_learning/src/components/0201_ichiran.vue
+++ 20230608_vue3_element_tmp_for_learning/src/components/0201_ichiran.vue
@@ -2,25 +2,19 @@
     <div>fwe</div>
     <div>
         <h1></h1>
-        <!-- <li v-for="application in applications" :key="application.appUketsukeNo">
-            {{ application }}
-        </li> -->
-        <!-- <li v-for="notification in notifacations" :key="notification.ntcID">
-            {{ notification }}
-        </li> -->
+        <div class="demo-pagination-block">
+            <div class="demonstration">All combined</div>
+            <el-pagination v-model:current-page="currentPage4" v-model:page-size="pageSize4"
+                :page-sizes="[100, 200, 300, 400]" :small="small" :disabled="disabled" :background="background"
+                layout="total, sizes, prev, pager, next, jumper" :total="400" @size-change="handleSizeChange"
+                @current-change="handleCurrentChange" />
+        </div>
         <el-form>
 
         </el-form>
         <div>
             {{ applicationPageCount }}
         </div>
-        <!-- <div class="demo-pagination-block">
-            <div class="demonstration">All combined</div>
-            <el-pagination v-model:current-page="currentPage" v-model:page-size="applicationPageCount"
-                :page-sizes="[10, 20, 50, 100, 200, 500]" :small="small" :disabled="disabled" :background="background"
-                layout="total, sizes, prev, pager, next, jumper" :total="applicationPageCount" @size-change="handleSizeChange"
-                @current-change="handleCurrentChange" />
-        </div> -->
     </div>
 </template>
 <script setup>
@@ -32,7 +26,7 @@ const applications = ref('');
 const notifacations = ref('');
 const applicationPageCount = ref('');
 const applicationPageSize = ref(10);
-// const currentPage = ref(1);
+const currentPage = ref(1);
 const getApplicationPage = (pageNum) => {
     axios.get('http://localhost:8815/Application/page/' + pageNum + '/' + applicationPageSize.value).then(response => {
         console.log(response);
@@ -40,16 +34,16 @@ const getApplicationPage = (pageNum) => {
     })
 };
 onMounted(() => {
-    axios.get('http://localhost:8815/Application/all').then(response => {
-        // for test add by ZHY
-        // console.log(response)
-        if (response.status == '200') {
-            applications.value = response.data.data;
+    // axios.get('http://localhost:8815/Application/all').then(response => {
+    //     // for test add by ZHY
+    //     // console.log(response)
+    //     if (response.status == '200') {
+    //         applications.value = response.data.data;
 
-            //for test add by ZHY
-            // console.log(this.applications);
-        }
-    });
+    //         //for test add by ZHY
+    //         // console.log(this.applications);
+    //     }
+    // });
     axios.get('http://localhost:8815/Notification/all').then(response => {
         // console.log(response);
         if (response.status == '200') {
@@ -77,13 +71,13 @@ onMounted(() => {
 //     }
 // }
 
-const currentPage1 = ref(5)
-const currentPage2 = ref(5)
-const currentPage3 = ref(5)
-const currentPage4 = ref(4)
-const pageSize2 = ref(100)
-const pageSize3 = ref(100)
-const pageSize4 = ref(100)
+// const currentPage1 = ref(5)
+// const currentPage2 = ref(5)
+// const currentPage3 = ref(5)
+// const currentPage4 = ref(4)
+// const pageSize2 = ref(100)
+// const pageSize3 = ref(100)
+// const pageSize4 = ref(100)
 const small = ref(false)
 const background = ref(false)
 const disabled = ref(false)
